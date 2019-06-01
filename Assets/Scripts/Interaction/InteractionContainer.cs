85449d82f98889ac88b9510c6d04df11863ee19c
PunkCG <punkcomputergraphics@gmail.com>
2019-06-01 15:36:05 -0500

    Added Interaction
    Added interaction behaviours.

---
+++ Assets/Scripts/Interaction/InteractionContainer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionContainer : MonoBehaviour
+{
+    public bool loopModels = true;
+    public List<GameObject> interactionModels = new List<GameObject>();
+    int actualModel = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Transform t in transform)
+        {
+            interactionModels.Add(t.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    public void InitModel()
+    {
+        ChangeModel(actualModel);
+    }
+
+    void ChangeModel(int modelIndex)
+    {
+        foreach(GameObject g in interactionModels)
+        {
+            g.SetActive(false);
+        }
+        //
+        interactionModels[actualModel].SetActive(true);
+    }
+    public void ChangeToNextModel()
+    {
+        if (actualModel + 1 < interactionModels.Count)
+        {
+            actualModel++;
+        }
+        else
+        {
+            if (loopModels)
+            {
+                actualModel = 0;
+            }
+        }
+        //
+        ChangeModel(actualModel);
+    }
+    public void ChangeToPrevModel()
+    {
+        if (actualModel - 1 >= 0)
+        {
+            actualModel--;
+        }
+        else
+        {
+            if (loopModels)
+            {
+                actualModel = interactionModels.Count - 1;
+            }
+        }
+        //
+        ChangeModel(actualModel);
+    }
+}
---
+++ Assets/Scripts/Interaction/InteractionElement.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionElement : MonoBehaviour
+{
+    [Header("Configuration")]
+    public InteractionType interactionType1 = InteractionType.ChangeColor;
+    public InteractionType interactionType2 = InteractionType.ChangeColor;
+    Material interactionMaterial;
+    MeshRenderer interactionMesh;
+
+    [Header("Change Color")]
+    public bool loopColors = true;
+    public List<Color> availableColors = new List<Color>();
+    int actualColor = 0;
+
+    [Header("Change Material")]
+    public bool loopMaterials = true;
+    public List<Material> availableMaterials = new List<Material>();
+    int actualMaterial = 0;
+
+    [Header("Change Model")]
+    public InteractionContainer interactionContainer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactionMesh = GetComponent<MeshRenderer>();
+        interactionMaterial = interactionMesh.material;
+
+        if(!interactionContainer)
+        {
+            interactionContainer = transform.parent.GetComponent<InteractionContainer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        switch (interactionType1)
+        {
+            case InteractionType.ChangeColor:
+                ChangeColor(actualColor);
+                break;
+            case InteractionType.ChangeMaterial:
+                ChangeMaterial(actualMaterial);
+                break;
+            case InteractionType.ChangeModel:
+                interactionContainer.InitModel();
+                break;
+        }
+    }
+
+    #region Interaction
+
+    public void Interaction1()
+    {
+        switch (interactionType1)
+        {
+            case InteractionType.ChangeColor:
+                ChangeToNextColor();
+                break;
+            case InteractionType.ChangeMaterial:
+                ChangeToNextMaterial();
+                break;
+            case InteractionType.ChangeModel:
+                interactionContainer.ChangeToNextModel();
+                break;
+        }
+    }
+
+    //Colors
+
+    void ChangeColor(int colorIndex)
+    {
+        interactionMaterial.SetColor("_BaseColor", availableColors[colorIndex]);
+    }
+    public void ChangeToNextColor()
+    {
+        if(actualColor + 1 < availableColors.Count)
+        {
+            actualColor++;
+        }
+        else
+        {
+            if(loopColors)
+            {
+                actualColor = 0;
+            }
+        }
+        //
+        ChangeColor(actualColor);
+    }
+    public void ChangeToPrevColor()
+    {
+        if (actualColor - 1 >= 0)
+        {
+            actualColor--;
+        }
+        else
+        {
+            if (loopColors)
+            {
+                actualColor = availableColors.Count - 1;
+            }
+        }
+        //
+        ChangeColor(actualColor);
+    }
+
+    //Materials
+
+    void ChangeMaterial(int matIndex)
+    {
+        interactionMesh.material = availableMaterials[matIndex];
+    }
+    public void ChangeToNextMaterial()
+    {
+        if (actualMaterial + 1 < availableMaterials.Count)
+        {
+            actualMaterial++;
+        }
+        else
+        {
+            if (loopMaterials)
+            {
+                actualMaterial = 0;
+            }
+        }
+        //
+        ChangeMaterial(actualMaterial);
+    }
+    public void ChangeToPrevMaterial()
+    {
+        if (actualMaterial - 1 >= 0)
+        {
+            actualMaterial--;
+        }
+        else
+        {
+            if (loopMaterials)
+            {
+                actualMaterial = availableMaterials.Count - 1;
+            }
+        }
+        //
+        ChangeMaterial(actualMaterial);
+    }
+
+    #endregion
+}
+public enum InteractionType
+{
+    ChangeMaterial,
+    ChangeColor,
+    ChangeModel
+}
